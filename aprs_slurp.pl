ea677b319a22d7168243c92ee5b25aa78bd46be3
Tony Collen <tony@halogenlabs.com>
2018-12-03 19:31:56 -0600

    Add looping and connection try/catch

--- aprs_slurp.pl
+++ aprs_slurp.pl
@@ -8,6 +8,8 @@ use POSIX;
 use Net::AMQP::RabbitMQ;
 use utf8;
 use Encode qw( encode_utf8 );
+
+use Try::Tiny;
 #use Net::Statsd;
 
 #$Net::Statsd::HOST = $ENV{'STATSD_HOST'} || "localhost";
@@ -20,64 +22,74 @@ my $rabbitmq_user = $ENV{'RABBITMQ_USERNAME'} || "guest";
 my $rabbitmq_password = $ENV{'RABBITMQ_PASSWORD'} || "guest";
 my $rabbitmq_vhost = $ENV{'RABBITMQ_VHOST'} || "aprs";
 my $rabbitmq_port = $ENV{'RABBITMQ_PORT'} || 5672;
-
 my $aprs_username = $ENV{'APRS_USERNAME'} || die "Set APRS_USERNAME to URCALL-SSID";
-
-warn "About to connect to RabbitMQ at $rabbitmq_host:$rabbitmq_port\n";
-
-$mq->connect($rabbitmq_host, { user => $rabbitmq_user, password => $rabbitmq_password, vhost => $rabbitmq_vhost, port => $rabbitmq_port });
-
-warn "Connected to RabbitMQ";
-my $json = JSON->new->allow_nonref;
-
 my $aprs_server = $ENV{'APRS_SERVER'} || "204.110.191.245:10152";
 
-warn "About to connect to APRS-IS server $aprs_server as $aprs_username";
 
-my $is = new Ham::APRS::IS($aprs_server, $aprs_username, 'appid' => 'aprs.me 0.1.0');
-$is->connect('retryuntil' => 3) || die "Failed to connect: $is->{error}";
-
-#p $is->{error};
-
-my $channel = 1;
-
-$mq->channel_open($channel);
-$mq->exchange_declare($channel, "aprs:messages", {exchange_type => 'topic'});
-$mq->queue_declare($channel, "aprs:archive", {durable => 1, auto_delete => 0});
-$mq->queue_bind($channel, "aprs:archive", "aprs:messages", '#', {});
-
-until (0)
+until(0)
 {
-  my $l = $is->getline_noncomment(1);
-  if (!defined $l) {
-    #Net::Statsd::increment('aprs.ingest.connectionlost');
-    print "\n[no connection, reconnecting]\n";
-    $is->disconnect();
-    $is->connect('retryuntil' => 100);
-    next;
+  try {
+    warn "About to connect to RabbitMQ at $rabbitmq_host:$rabbitmq_port\n";
+
+    $mq->connect($rabbitmq_host, { user => $rabbitmq_user, password => $rabbitmq_password, vhost => $rabbitmq_vhost, port => $rabbitmq_port });
+
+    warn "Connected to RabbitMQ";
+    my $json = JSON->new->allow_nonref;
+
+    warn "About to connect to APRS-IS server $aprs_server as $aprs_username";
+
+    my $is = new Ham::APRS::IS($aprs_server, $aprs_username, 'appid' => 'aprs.me 0.1.0');
+    $is->connect('retryuntil' => 3) || die "Failed to connect: $is->{error}";
+
+    #p $is->{error};
+
+    my $channel = 1;
+
+    $mq->channel_open($channel);
+    $mq->exchange_declare($channel, "aprs:messages", {exchange_type => 'topic'});
+    $mq->queue_declare($channel, "aprs:archive", {durable => 1, auto_delete => 0});
+    $mq->queue_bind($channel, "aprs:archive", "aprs:messages", '#', {});
+
+    until (0)
+    {
+      my $l = $is->getline_noncomment(1);
+      if (!defined $l) {
+        #Net::Statsd::increment('aprs.ingest.connectionlost');
+        print "\n[no connection, reconnecting]\n";
+        $is->disconnect();
+        $is->connect('retryuntil' => 100);
+        next;
+      }
+
+      my %packetdata;
+      my $retval = parseaprs($l, \%packetdata);
+      my $keys;
+      my $values;
+      my $jsonpacket;
+
+      if ($retval == 1)
+      {
+          #Net::Statsd::increment('aprs.ingest.packets.valid');
+          $jsonpacket = $json->encode(\%packetdata);
+          my $publish_key = "aprs." . $packetdata{srccallsign};
+          $mq->publish($channel, $publish_key, encode_utf8($jsonpacket), { exchange => "aprs:messages", persistent => 1});
+      }
+      else
+      {
+        if (exists($packetdata{resultmsg})) {
+          #Net::Statsd::increment('aprs.ingest.packets.invalid');
+          warn "Parsing failed: $packetdata{resultmsg} ($packetdata{resultcode})\n";
+        }
+      }
+    }
+
+    $mq->disconnect();
+    $is->disconnect() || die "Failed to disconnect: $is->{error}";
+  } catch {
+    warn "Caught error: $_, sleeping 3 seconds...";
+    sleep 3;
   }
-
-  my %packetdata;
-  my $retval = parseaprs($l, \%packetdata);
-  my $keys;
-  my $values;
-  my $jsonpacket;
-
-  if ($retval == 1)
-  {
-      #Net::Statsd::increment('aprs.ingest.packets.valid');
-      $jsonpacket = $json->encode(\%packetdata);
-      my $publish_key = "aprs." . $packetdata{srccallsign};
-      $mq->publish($channel, $publish_key, encode_utf8($jsonpacket), { exchange => "aprs:messages", persistent => 1});
-   }
-   else
-   {
-     if (exists($packetdata{resultmsg})) {
-       #Net::Statsd::increment('aprs.ingest.packets.invalid');
-       warn "Parsing failed: $packetdata{resultmsg} ($packetdata{resultcode})\n";
-     }
-   }
+    
 }
 
-$mq->disconnect();
-$is->disconnect() || die "Failed to disconnect: $is->{error}";
+
